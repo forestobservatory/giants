23a0d0c8d79b517c8dd01659cbc5cab2e210faaa
earth-chris <cba@salo.ai>
2021-09-11 00:15:41 -0700

    installer init

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+version = open("giants/__version__.py").read().strip('"\n')
+long_description = open("README.md", "r", encoding="utf-8").read()
+required = open("requirements.txt", "r", encoding="utf-8").read().strip().split()
+
+setup(
+    name="giants",
+    url="https://the.forestobservatory.com/giants",
+    author="Salo Sciences",
+    author_email="cba@salo.ai",
+    license="MIT",
+    description="Statistical and geospatial modeling tools for mapping big trees.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[
+        "ecology",
+        "conservation",
+        "remote sensing",
+        "machine learning",
+    ],
+    packages=find_packages(exclude="tests"),
+    version=version,
+    python_requires=">=3.0",
+    platforms="any",
+    install_requires=required,
+    include_package_data=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
